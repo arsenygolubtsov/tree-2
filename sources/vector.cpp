0c6a07599257a5c83df4bfa08082ef47312e23ca
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2018-03-16 13:09:20 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -26,7 +26,7 @@ vector_t & vector_t::operator =(vector_t const & other)
         delete [] elements_;
         size_ = other.size_;
         capacity_ = other.capacity_;
-        elements_ = new int[size_];
+        elements_ = new int[capacity_];
         for(std::size_t i = 0; i < size_; ++i){
             elements_[i] = other.elements_[i];
         }
@@ -36,14 +36,15 @@ vector_t & vector_t::operator =(vector_t const & other)
 
 bool vector_t::operator ==(vector_t const & other) const
 {
-    if(size_ == other.size_){
+bool success = true;
+    if(size_ == other.size_ && capacity_ == other.capacity_){
         for(std::size_t i = 0; i < size_; ++i){
             if(elements_[i] != other.elements_[i]){
-                return false;
+                success = false; break;
             }
         }
     }
-	return true;
+	return success;
 }
 
 vector_t::~vector_t()
