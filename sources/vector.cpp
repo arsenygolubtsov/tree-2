8b87bb1833e876b5da53d22f2ab6a76875f3e229
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2018-03-16 12:16:03 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -5,24 +5,49 @@
 
 vector_t::vector_t()
 {
+    int *elements_ = nullptr;
+    std::size_t size_ = 0;
+    std::size_t capacity_ = 0;
 }
 
 vector_t::vector_t(vector_t const & other)
 {
+    size_ = other.size_;
+    capacity_ = other.capacity_;
+    elements_ = new int[capasity];
+    for(std::size_t i = 0; i < size_; ++i){
+        elements_[i] = other.elements_[i];
 }
 
 vector_t & vector_t::operator =(vector_t const & other)
 {
+    if(this != &other){
+        delete [] elements_;
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+        elements_ = new int[size_];
+        for(std::size_t i = 0; i < size_; ++i){
+            elements_[i] = other.elements_[i];
+        }
+    }
 	return *this;
 }
 
 bool vector_t::operator ==(vector_t const & other) const
 {
-	return false;
+    if(size_ == other.size_){
+        for(std::size_t i = 0; i < size_; ++i){
+            if(elements_[i] != other.elements_[i]){
+                return false;
+            }
+        }
+    }
+	return true;
 }
 
 vector_t::~vector_t()
 {
+    delete [] elements_;
 }
 
 std::size_t vector_t::size() const
@@ -37,10 +62,53 @@ std::size_t vector_t::capacity() const
 
 void vector_t::push_back(int value)
 {
+    if(capacity_ == 0){
+        capacity_ = 1;
+        size_ = 1;
+        elements_ = new int[capacity_];
+        elements_[0] = value;
+    }
+    else{
+        if(size_ == capacity_){
+            int *massive;
+            massive = new int[size_];
+            for(std::size_t i = 0; i < size_; ++i){
+                massive[i] = elements_[i];
+            }
+            delete [] elements_;
+            capacity_ = 2 * capacity_;
+            elements_ = new int[capacity_];
+            for(std::size_t i = 0, i < size_; ++i){
+                elements_[i] = massive[i];
+            }
+            delete [] massive;
+            elements_[size_] = value;
+            size_ ++;
+        }
+        else{
+            elements_[size_] = value;
+            size_ ++;
+        }
+    }
 }
 
 void vector_t::pop_back()
 {
+    size_ --;
+    if(size_ == 0 || size_ * 4 == capacity_){
+        int *massive;
+        massive = new int[size_];
+        for(std::size_t i = 0; i < size_; ++i){
+            massive[i] = elements_[i];
+        }
+        delete [] elements_;
+        capacity_ = capacity_ / 2;
+        elements_ = new int[capacity_];
+        for(std::size_t i = 0, i < size_; ++i){
+            elements_[i] = massive[i];
+        }
+        delete [] massive;
+    }        
 }
 
 int & vector_t::operator [](std::size_t index)
@@ -55,5 +123,9 @@ int vector_t::operator [](std::size_t index) const
 
 bool operator !=(vector_t const & lhs, vector_t const & rhs)
 {
-	return true;
+	bool success = true;
+	if (lhs == rhs) {
+		success = !success;
+	}
+	return success;
 }
