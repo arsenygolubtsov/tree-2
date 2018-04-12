cb336814ac62ecf37ef392972221eac53aa31a27
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2018-04-13 01:09:14 +0300

    Update tree.cpp

--- tests/tree.cpp
+++ tests/tree.cpp
@@ -5,13 +5,13 @@
 
 TEST_CASE("creating tree")
 {
-    tree_t tree;
+    tree_t <int> tree;
     REQUIRE(tree.root() == nullptr);
 }
 
-TEST_CASE("insert and print tree")
+TEST_CASE("insert and print tree int")
 {
-    tree_t tree;
+    tree_t <int> tree;
 
     std::string input1{ "+3\n" };
     std::string input2{ "+4\n" };
@@ -41,9 +41,41 @@ TEST_CASE("insert and print tree")
     REQUIRE(ostream.str() == result);
 }
 
-TEST_CASE("find tree")
+TEST_CASE("insert and print tree double")
 {
-    tree_t tree;
+    tree_t <double> tree;
+
+    std::string input1{ "+3.3\n" };
+    std::string input2{ "+4.4\n" };
+    std::string input3{ "+2.2\n" };
+    std::string input4{ "+1.1\n" };
+    std::string input5{ "+5.5\n" };
+    std::string result{ "------5.5\n"
+                        "----4.4\n"
+                        "--3.3\n"
+                        "----2.2\n"
+                        "------1.1\n" };
+
+    std::istringstream inp1{ input1 };
+    std::istringstream inp2{ input2 };
+    std::istringstream inp3{ input3 };
+    std::istringstream inp4{ input4 };
+    std::istringstream inp5{ input5 };
+
+    REQUIRE(read(tree, inp1));
+    REQUIRE(read(tree, inp2));
+    REQUIRE(read(tree, inp3));
+    REQUIRE(read(tree, inp4));
+    REQUIRE(read(tree, inp5));
+
+    std::ostringstream ostream;
+    tree.print(ostream, tree.root(), 1);
+    REQUIRE(ostream.str() == result);
+}
+
+TEST_CASE("find tree int")
+{
+    tree_t <int> tree;
 
     std::string input1{ "+3\n" };
     std::string input2{ "+4\n" };
@@ -68,4 +100,86 @@ TEST_CASE("find tree")
     REQUIRE(tree.find(5) == true);
     REQUIRE(tree.find(7) == false);
     REQUIRE(tree.find(0) == false);
+}
+
+TEST_CASE("find tree double")
+{
+    tree_t <double> tree;
+
+    std::string input1{ "+3.3\n" };
+    std::string input2{ "+4.4\n" };
+    std::string input3{ "+2.2\n" };
+    std::string input4{ "+1.1\n" };
+    std::string input5{ "+5.5\n" };
+
+    std::istringstream inp1{ input1 };
+    std::istringstream inp2{ input2 };
+    std::istringstream inp3{ input3 };
+    std::istringstream inp4{ input4 };
+    std::istringstream inp5{ input5 };
+
+    REQUIRE(read(tree, inp1));
+    REQUIRE(read(tree, inp2));
+    REQUIRE(read(tree, inp3));
+    REQUIRE(read(tree, inp4));
+    REQUIRE(read(tree, inp5));
+
+    REQUIRE(tree.find(1.1) == true);
+    REQUIRE(tree.find(2.2) == true);
+    REQUIRE(tree.find(5.5) == true);
+    REQUIRE(tree.find(7.7) == false);
+    REQUIRE(tree.find(0) == false);
+}
+
+TEST_CASE("equaling trees")
+{
+    	tree_t <int> tree1, tree2;
+    	tree1.insert(3);
+    	tree1.insert(4);
+    	tree1.insert(2);
+    	tree2.insert(3);
+    	tree2.insert(4);
+    	tree2.insert(2);
+	    
+	    REQUIRE(tree1==tree2);
+	    
+	    tree2.insert(1);
+    	
+    	REQUIRE((tree1==tree2) == false);
+}
+
+TEST_CASE("deleting")
+{
+    	tree_t<int> tree{3, 4, 2, 1, 5, 10, 7, 8};
+	    std::string result{ "--------10\n"
+	                        "------------8\n"
+                            "----------7\n"
+                            "------5\n"
+                            "----4\n"
+                            "--3\n"
+                            "----2\n"
+                            "------1\n"};
+    	
+    	tree.remove(int 5);
+    	std::string result5{"------10\n"
+	                        "----------8\n"
+                            "--------7\n"
+                            "----4\n"
+                            "--3\n"
+                            "----2\n"
+                            "------1\n"};
+	    std::ostringstream ostream1;
+	    tree.print(ostream1, tree.root(), 1);
+	    REQUIRE( ostream1.str() == result5 );
+	       
+	    tree.remove(int 1);
+    	std::string result1{"------10\n"
+	                        "----------8\n"
+                            "--------7\n"
+                            "----4\n"
+                            "--3\n"
+                            "----2\n"};
+	    std::ostringstream ostream2;
+	    tree.print(ostream2, tree.root(), 1);
+	    REQUIRE( ostream2.str() == result1 );
 }
